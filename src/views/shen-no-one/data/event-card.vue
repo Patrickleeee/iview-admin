242dea68d62d4b893c61807e1664b561ec951005
Patrick Leee <patrickleee@foxmail.com>
2018-01-17 19:39:57 +0800

    时间轴

---
+++ src/views/shen-no-one/data/event-card.vue
@@ -0,0 +1,55 @@
+<template>
+    <Row style="background:#eee;padding:20px">
+        <Col span="8">
+            <Card>
+                <div style="text-align:center">
+                <img class="avator-img" src="../../../images/Bichon.gif" />
+            </div>
+            </Card> 
+        </Col>
+        <Col span="15" offset="1">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent euismod fringilla nibh, feugiat pretium velit. Integer elit urna, maximus vitae rutrum hendrerit, pretium sed massa.
+        </Col>
+    </Row>
+</template>
+<script>
+    export default {
+        name: 'eventCard',
+        data () {
+            return {
+                movieList: [
+                    {
+                        name: '累计借款数量',
+                        url: 'https://movie.douban.com/subject/1292052/',
+                        rate: 9.6
+                    },
+                    {
+                        name: '逾期数量',
+                        url: 'https://movie.douban.com/subject/1295644/',
+                        rate: 9.4
+                    },
+                    {
+                        name: '待还款订单数量',
+                        url: 'https://movie.douban.com/subject/1291546/',
+                        rate: 9.5
+                    },
+                    {
+                        name: '逾期订单数量',
+                        url: 'https://movie.douban.com/subject/1292720/',
+                        rate: 9.4
+                    },
+                    {
+                        name: '待还款总金额',
+                        url: 'https://movie.douban.com/subject/1292063/',
+                        rate: 9.5
+                    },
+                    {
+                        name: '逾期总金额',
+                        url: 'https://movie.douban.com/subject/1291561/',
+                        rate: 9.2
+                    }
+                ]
+            };
+        }
+    };
+</script>
--- src/views/shen-no-one/major-event.vue
+++ src/views/shen-no-one/major-event.vue
@@ -1,42 +1,230 @@
 <style scoped>
-    .time{
-        font-size: 14px;
-        font-weight: bold;
-    }
-    .content{
-        padding-left: 5px;
-    }
+    .demo{ background: #e7e7e7; padding: 1.5em 0}
+		.main-timeline{
+		    position: relative;
+		    -webkit-transition: all 0.4s ease 0s;
+		    -moz-transition: all 0.4s ease 0s;
+		    -ms-transition: all 0.4s ease 0s;
+		    transition: all 0.4s ease 0s;
+		}
+		.main-timeline:before{
+		    content: "";
+		    width: 3px;
+		    height: 100%;
+		    background: #1abc9c;
+		    position: absolute;
+		    top: 0;
+		    left: 50%;
+		}
+		.main-timeline .timeline{
+		    margin-bottom: 50px;
+		    position: relative;
+		}
+		.main-timeline .timeline:before,
+		.main-timeline .timeline:after{
+		    content: "";
+		    display: block;
+		    width: 100%;
+		    clear: both;
+		}
+		.main-timeline .timeline-icon{
+		    width: 50px;
+		    height: 50px;
+		    border-radius: 50%;
+		    background: #1abc9c;
+		    overflow: hidden;
+		    margin-left: -23px;
+		    position: absolute;
+		    top: 0;
+		    left: 50%;
+		    text-align: center;
+		}
+		.main-timeline .timeline-icon i{
+		    font-size: 3px;
+		    line-height: 50px;
+		    color: #fff;
+		}
+		.main-timeline .timeline-content{
+		    width: 45%;
+		    padding: 20px;
+		    border-radius: 5px;
+		    background: #fff;
+		    -webkit-box-shadow: 0 3px 0 rgba(0, 0, 0, 0.1);
+		    -moz-box-shadow: 0 3px 0 rgba(0, 0, 0, 0.1);
+		    -ms-box-shadow: 0 3px 0 rgba(0, 0, 0, 0.1);
+		    box-shadow: 0 3px 0 rgba(0, 0, 0, 0.1);
+		    -webkit-transition: all 0.3s ease 0s;
+		    -moz-transition: all 0.3s ease 0s;
+		    -ms-transition: all 0.3s ease 0s;
+		    transition: all 0.3s ease 0s;
+		}
+		.main-timeline .timeline-content:before{
+		    content: "";
+		    border-left: 7px solid #1abc9c;
+		    border-top: 7px solid transparent;
+		    border-bottom: 7px solid transparent;
+		    position: absolute;
+		    left: 45%;
+		    top: 20px;
+		}
+		.main-timeline .title{
+		    font-size: 30px;
+		    font-weight: 300;
+		    color: #fff;
+		    padding: 10px;
+		    background: #1abc9c;
+		    border-radius: 3px 3px 0 0;
+		    margin: -20px -20px 10px;
+		}
+		.main-timeline .description{
+		    font-size: 14px;
+		    color: #726f77;
+		}
+		.main-timeline .read-more{
+		    display: inline-block;
+		    font-size: 12px;
+		    color: #64d8c1;
+		    text-transform: uppercase;
+		    padding: 5px 15px;
+		    border: 2px solid #64d8c1;
+		    position: relative;
+		    border-radius: 5px;
+		    -webkit-box-shadow: 2px 2px 0 #64d8c1;
+		    -moz-box-shadow: 2px 2px 0 #64d8c1;
+		    -ms-box-shadow: 2px 2px 0 #64d8c1;
+            box-shadow: 2px 2px 0 #64d8c1;
+            margin: 2px;
+		}
+		.main-timeline .read-more:hover{
+		    top: 2px;
+		    left: 2px;
+		    box-shadow: none;
+		}
+		.main-timeline .timeline-content.right{
+		    float: right;
+		}
+		.main-timeline .timeline-content.right:before{
+		    content: "";
+		    right: 45%;
+		    left: inherit;
+		    border-left: 0;
+		    border-right: 7px solid #1abc9c;
+		}
+		@media only screen and (max-width: 990px){
+		    .main-timeline .title{
+		        font-size: 25px;
+		    }
+		    .main-timeline .timeline-content::before{
+		        top: 16px;
+		    }
+		}
+		@media only screen and (max-width: 767px){
+		    .main-timeline{ margin-left: 20px; }
+		    .main-timeline:before{ left: 0; }
+		    .main-timeline .timeline-content{
+		        width: 90%;
+		        float: right;
+		    }
+		    .main-timeline .timeline-content:before,
+		    .main-timeline .timeline-content.right:before{
+		        left: 10%;
+		        right: inherit;
+		        margin-left: -6px;
+		        border-left: 0;
+		        border-right: 7px solid #1abc9c;
+		    }
+		    .main-timeline .timeline-icon{
+		        left: 0;
+		    }
+		}
+		@media only screen and (max-width: 479px){
+		    .main-timeline .timeline-content{
+		        width: 85%;
+		    }
+		    .main-timeline .timeline-content:before,
+		    .main-timeline .timeline-content.right:before{
+		        left: 15%;
+		    }
+		    .main-timeline .title{
+		        font-size: 20px;
+		    }
+		    .main-timeline .timeline-content:before{
+		        top: 13px;
+		    }
+		}
 </style>
 <template>
-    <Timeline pending>
-        <TimelineItem>
-            
-            <p class="time">1976年</p>
-            <p class="content">Apple I 问世</p>
-        </TimelineItem>
-        <TimelineItem>
-            <p class="time">1984年</p>
-            <p class="content">发布 Macintosh</p>
-        </TimelineItem>
-        <TimelineItem>
-            <p class="time">2007年</p>
-            <p class="content">发布 iPhone</p>
-        </TimelineItem>
-        <TimelineItem>
-            <p class="time">2010年</p>
-            <p class="content">发布 iPad</p>
-        </TimelineItem>
-        <TimelineItem>
-            <p class="time">2011年10月5日</p>
-            <p class="content">史蒂夫·乔布斯去世</p>
-        </TimelineItem>
-        <TimelineItem>
-            <a href="#">查看更多</a>
-        </TimelineItem>
-    </Timeline>
+    <div >
+    <Row class="margin-top-10">
+        <div class="demo">
+	        <div class="container">
+	            <div class="row">
+	                <div class="col-md-12">
+	                    <div class="main-timeline">
+	                        <div class="timeline">
+	                            <div class="timeline-icon">
+	                                <i class="fa fa-star">2018年</i>   
+	                            </div>
+	                            <div class="timeline-content">
+	                                <h2 class="title">Web Design</h2>
+	                                <p class="description">
+	                                    <event-card></event-card>
+	                                </p>
+	                                <a href="#" class="read-more">read more</a>
+	                            </div>
+	                        </div>
+
+	                        <div class="timeline">
+	                            <div class="timeline-icon">
+	                                <i class="fa fa-star">2014年</i>
+	                            </div>
+	                            <div class="timeline-content right">
+	                                <h2 class="title">Web Development</h2>
+	                                <p class="description">
+	                                    <event-card></event-card>
+	                                </p>
+	                                <a href="#" class="read-more">read more</a>
+	                            </div>
+	                        </div>
+
+	                        <div class="timeline">
+	                            <div class="timeline-icon">
+	                                <i class="fa fa-star">2010年</i>
+	                            </div>
+	                            <div class="timeline-content">
+	                                <h2 class="title">Brand Building</h2>
+	                                <p class="description">
+	                                    <event-card></event-card>
+	                                </p>
+	                                <a href="#" class="read-more">read more</a>
+	                            </div>
+	                        </div>
+
+	                        <div class="timeline">
+	                            <div class="timeline-icon">
+	                                <i class="fa fa-star">2008年</i>
+	                            </div>
+	                            <div class="timeline-content right">
+	                                <h2 class="title">Responsive Design</h2>
+	                                <p class="description">
+	                                    <event-card></event-card>
+	                                </p>
+	                                <a href="#" class="read-more">read more</a>
+	                            </div>
+	                        </div>
+	                    </div>
+	                </div>
+	            </div>
+	        </div>
+	    </div>
+    </Row>
+    </div>
 </template>
 <script>
-    export default {
-    
-    };
+import eventCard from './data/event-card.vue';
+export default {
+    components: {
+        eventCard
+    }
+};
 </script>
